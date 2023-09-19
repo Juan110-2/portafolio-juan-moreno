d32a9a8ef1b19cef6affb0982c40070a7f5248d8
Juan Jose Moreno <juan.moreno.jaramillo@correounivalle.edu.co>
2023-09-18 22:30:33 -0500

    Add Dragon Flying

---
+++ src/World/Dragon.jsx
@@ -0,0 +1,27 @@
+import { useAnimations, useGLTF } from "@react-three/drei";
+import { useEffect, useRef } from "react";
+
+// const { useGLTF, useAnimations } = require("@react-three/drei") -> Otra manera de importar
+
+const Dragon = () => {
+    const dragonRef = useRef();
+    const dragonModel = useGLTF('/assets/models/dragon/scene.gltf')
+    const { animations } = dragonModel;
+    const { actions } = useAnimations(animations, dragonRef);
+
+    // console.log(Object.keys(actions));
+
+       useEffect(() => {
+           const action = actions['Object_0']
+           action.play()
+       }, [])
+
+    return (
+        <mesh ref={dragonRef} position-x={0}  position-y={16} rotation-y={-Math.PI * 0.15} scale={0.1}>
+            <primitive object={dragonModel.scene} />
+        </mesh>
+    )
+}
+
+export default Dragon;
+useGLTF.preload("/assets/models/dragon/scene.gltf");
